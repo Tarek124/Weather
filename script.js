6ef2e2601481cbb5885b2a417f285d874dda0aa8
tarek <afrantarek123456789@gmail.com>
2023-08-15 12:25:02 +0600

    weather app done

---
+++ script.js
@@ -0,0 +1,43 @@
+const apiKey = '6fd3ce76aedb9ec7232f6603014c39d1',
+    apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=',
+    input = document.getElementById('input'),
+    btn = document.getElementById('btn'),
+    tem = document.getElementById('tem'),
+    citynum= document.getElementById('cityName'),
+    lead = document.getElementById("lead"),
+    img = document.getElementById("img")
+
+
+let citys = "feni";
+btn.addEventListener('click', () => {
+    citys = input.value
+    checkWeather()
+
+})
+async function checkWeather() {
+    const res = await fetch(apiUrl + citys + `&appid=${apiKey}`);
+    let data = await res.json();
+    e(data)
+}
+
+
+checkWeather()
+function e(data) {
+    let cityName = data.name;
+    let tempera = data.main.temp 
+    let feels_like = data.main.feels_like
+    let sky = data.weather[0].main;
+    let icons = data.weather[0].icon
+    citynum.innerText = cityName
+    tem.innerText = Math.floor(tempera);
+    lead.innerText = sky;
+    img.src = `https://openweathermap.org/img/wn/${icons}@2x.png` 
+
+}
+
+
+
+
+// Precipitation: 40%
+// Humidity: 84%
+// Wind: 11 km/h
